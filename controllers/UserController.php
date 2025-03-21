42f73b480e587a4323ed87e0419121673eb6d8de
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-03-21 14:04:54 -0400

    create UserController and method create

---
+++ controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controllers;
+
+// use App\Models\User;
+use App\Providers\View;
+
+// use App\Providers\Auth;
+
+class UserController
+{
+    public function create()
+    {
+        return View::render(['usercreate']);
+    }
+
+}
