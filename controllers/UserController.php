add8c6c0ff3dda0bbfb22715b28950958a79a75b
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-03-22 17:13:57 -0400

    add user update in Controller

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,9 +2,8 @@
 namespace App\Controllers;
 
 use App\Models\User;
-use App\Providers\View;
-
 use App\Providers\Auth;
+use App\Providers\View;
 
 class UserController
 {
@@ -18,7 +17,7 @@ class UserController
         $user                 = new User;
         $data['username']     = $data["email"];
         $data["privilege_id"] = '0';
-        $data['password'] = $user->hashPassword($data['password'], 15);
+        $data['password']     = $user->hashPassword($data['password'], 15);
         $insert               = $user->insert($data);
         if ($insert) {
             return view::redirect('user/show');
@@ -30,16 +29,56 @@ class UserController
     public function show()
     {
         if (Auth::session()) {
-        if (isset($_SESSION['user_id'])) {
-            $user = new User;
-            if ($selectUser = $user->selectId($_SESSION['user_id'])) {
-                return View::render('user/show', ['user' => $selectUser]);
+            if (isset($_SESSION['user_id'])) {
+                $user = new User;
+                if ($selectUser = $user->selectId($_SESSION['user_id'])) {
+                    return View::render('user/show', ['user' => $selectUser]);
+                } else {
+                    return View::render('error', ['msg' => 'L\'utilisateur n\'existe pas']);
+                }
             } else {
-                return View::render('error', ['msg' => 'L\'utilisateur n\'existe pas']);
+                return view::redirect('login');
             }
-        } else{
-            return View::render('error', ['msg' => 'Inscrivez-vous!']);
         }
     }
-}
+
+    public function edit()
+    {
+        if (Auth::session()) {
+            if (isset($_SESSION['user_id'])) {
+                $user = new User;
+                if ($selectId = $user->selectId($_SESSION['user_id'])) {
+                    return View::render('user/edit', ['user' => $selectId]);
+                } else {
+                    return View::render('error', ['msg' => 'L\'utilisateur n\'existe pas']);
+                }
+            } else {
+                return view::redirect('login');
+            }
+        } else {
+            return view::redirect('login');
+        }
+    }
+
+    public function update($data)
+    {
+
+        // TODO: faire validation avec clause comme password dessous
+        if (Auth::session()) {
+
+            $user = new User;
+            if (! empty(trim($data["password"])) && strlen($data["password"]) >= 5) {
+                $data['password'] = $user->hashPassword($data['password'], 15);
+            } else {
+                unset($data['password']);
+            }
+
+            $update = $user->update($data, $_SESSION['user_id']);
+            if ($update) {
+                return view::redirect('user/show');
+            } else {
+                return View::render('error', ['msg' => 'Impossible d\'appliquer les modifications']);
+            }
+        }
+    }
 }
