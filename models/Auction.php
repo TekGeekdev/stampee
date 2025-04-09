8a060e7f078265b647966ef4ebc3213b2beaf837
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-04-09 18:58:23 -0400

    add filter color and date min & max

--- models/Auction.php
+++ models/Auction.php
@@ -9,13 +9,13 @@ class Auction extends CRUD
     protected $primaryKey = 'id';
     protected $fillable   = ['name', 'dateStart', 'dateFinish', 'startPrice', 'lordLike', 'state_id', 'stamp_id'];
 
-
-    public function filter($filters){
+    public function filter($filters)
+    {
         $params = [];
-        $sql = "SELECT auction.id, auction.name, 
-					auction.dateStart, 
-					auction.dateFinish, 
-                    auction.startPrice, 
+        $sql    = "SELECT auction.id, auction.name,
+					auction.dateStart,
+					auction.dateFinish,
+                    auction.startPrice,
                     auction.lordLike,
                     auction.state_id,
                     auction.stamp_id,
@@ -32,36 +32,45 @@ class Auction extends CRUD
                     stamp.content
                     FROM auction INNER JOIN stamp ON auction.stamp_id = stamp.id WHERE true";
 
-            if (!empty($filters["conditions_id"])) {
-                $sql .= " AND conditions_id = :conditions_id";
-                $params["conditions_id"] = $filters["conditions_id"];
-            }
+        if (! empty($filters["conditions_id"])) {
+            $sql .= " AND conditions_id = :conditions_id";
+            $params["conditions_id"] = $filters["conditions_id"];
+        }
 
-            if (!empty($filters["countryOrigin_id"])) {
-                $sql .= " AND countryOrigin_id = :countryOrigin_id";
-                $params["countryOrigin_id"] = $filters["countryOrigin_id"];
-            }
+        if (! empty($filters["countryOrigin_id"])) {
+            $sql .= " AND countryOrigin_id = :countryOrigin_id";
+            $params["countryOrigin_id"] = $filters["countryOrigin_id"];
+        }
 
-            if (!empty($filters["countryOrigin_id"])) {
-                $sql .= " AND countryOrigin_id = :countryOrigin_id";
-                $params["countryOrigin_id"] = $filters["countryOrigin_id"];
-            }
+        if (! empty($filters["color_id"])) {
+            $sql .= " AND color_id = :color_id";
+            $params["color_id"] = $filters["color_id"];
+        }
 
-            if (!empty($filters["minimum"])) {
-                $sql .= " AND startPrice >= :minimum";
-                $params["minimum"] = $filters["minimum"];
-            }
+        if (! empty($filters["minimum"])) {
+            $sql .= " AND startPrice >= :minimum";
+            $params["minimum"] = $filters["minimum"];
+        }
 
-            if (!empty($filters["maximum"])) {
-                $sql .= " AND startPrice <= :maximum";
-                $params["maximum"] = $filters["maximum"];
-            }
+        if (! empty($filters["maximum"])) {
+            $sql .= " AND startPrice <= :maximum";
+            $params["maximum"] = $filters["maximum"];
+        }
+
+        if (! empty($filters["minimumDate"])) {
+            $sql .= " AND dateRelease >= :minimumDate";
+            $params["minimumDate"] = $filters["minimumDate"];
+        }
+
+        if (! empty($filters["maximumDate"])) {
+            $sql .= " AND dateRelease <= :maximumDate";
+            $params["maximumDate"] = $filters["maximumDate"];
+        }
 
 
-            // echo($sql);
-            $stmt = parent::prepare($sql);
-            $stmt->execute($params);
-            return $stmt->fetchAll();
+        // echo($sql);
+        $stmt = parent::prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll();
     }
 }
-
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::get('/auction', 'AuctionController@index');
 Route::get('/auction/show', 'AuctionController@show');
 Route::get('/auction/filter', 'AuctionController@filter');
 
+Route::get('/auction/lord', 'AuctionController@indexLord');
+
 Route::get('/bid/store', 'BidController@store');
 Route::get('/bid/store/show', 'BidController@storeShow');
 
